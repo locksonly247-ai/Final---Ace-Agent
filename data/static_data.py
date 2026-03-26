9b75945e801ee466379e89fe51d27fda86109694
locksonly247-ai <locksonly247@gmail.com>
2026-03-26 03:10:19 -0700

    Add static_data.py

---
+++ data/static_data.py
@@ -0,0 +1,10 @@
+MATCHES = [
+    {
+        "players": ("Tommy Paul", "Arthur Fils"),
+        "time": "8:30 PM EST",
+    },
+    {
+        "players": ("Jiri Lehecka", "Martin Landaluce"),
+        "time": "3:00 PM EST",
+    }
+]
